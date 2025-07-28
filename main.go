faec61cd09a8f1cfbd787e92c54539f2ce4d2cbd
Saran Baskaran <tutysara@gmail.com>
2025-07-28 12:36:07 +0530

    feat: structured logging using zap

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/tutysara/banking-go/app"
+import (
+	"github.com/tutysara/banking-go/app"
+	"github.com/tutysara/banking-go/logger"
+)
 
 func main() {
+	logger.Info("Starting the application...")
 	app.Start()
 }
